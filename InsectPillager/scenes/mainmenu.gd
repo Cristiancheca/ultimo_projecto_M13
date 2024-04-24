7b79b7f985edf6905b00487f631a2ec699f429fa
cristian <118267798+Cristiancheca@users.noreply.github.com>
2024-04-24 10:48:23 +0200

    Menu opciones

--- InsectPillager/scenes/mainmenu.gd
+++ InsectPillager/scenes/mainmenu.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+onready var MainMenu = $Menu
+onready var Options = $Opciones
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -17,4 +18,45 @@ func _ready():
 
 
 func _on_play_pressed():
+<<<<<<< Updated upstream
 	get_tree().change_scene("res://scenes/level.tscn")
+=======
+	get_tree().change_scene("res://scenes/mercado.tscn")
+
+
+func _on_options_pressed():
+	show_and_hide(Options,MainMenu)
+
+
+func show_and_hide(first,Second):
+	first.show()
+	Second.hide()
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+func _on_Volver_al_menu_pressed():
+	show_and_hide(MainMenu,Options)
+
+
+func _on_CheckBox_toggled(button_pressed):
+	OS.window_fullscreen = button_pressed
+
+
+func _on_Master_value_changed(value):
+	Volumen(0,value)
+
+
+func Volumen(bus_index, value):
+	AudioServer.set_bus_volume_db(bus_index,value)
+
+
+func _on_Music_value_changed(value):
+	Volumen(1,value)
+
+
+func _on_Efects_value_changed(value):
+	Volumen(2,value)
+>>>>>>> Stashed changes
